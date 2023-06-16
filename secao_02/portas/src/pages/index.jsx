f7b9f82a4318bc911afa72ba2a0bcedb7005cb6d
Ruan-Portella <ruanmorales29@gmail.com>
2023-06-16 20:12:55 -0300

    Feat: Cria componente de porta

--- secao_02/portas/src/pages/index.jsx
+++ secao_02/portas/src/pages/index.jsx
@@ -1,9 +1,12 @@
+import Porta from "../components/Porta";
 import Present from "../components/Presente";
 
 export default function Home() {
   return (
-   <div>
-    <Present/>
+   <div style={{display: 'flex'}}>
+    {/* <Present/> */}
+    <Porta selecionada />
+    <Porta />
    </div>
   )
 }
