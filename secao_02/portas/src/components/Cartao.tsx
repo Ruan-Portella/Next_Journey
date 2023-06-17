b0a98bdcf5b29592884f01cb75a548617bbee018
Ruan-Portella <ruanmorales29@gmail.com>
2023-06-16 22:25:48 -0300

    Cria componente Cartão

---
+++ secao_02/portas/src/components/Cartao.tsx
@@ -0,0 +1,15 @@
+import styles from '../styles/Cartao.module.css'
+import React from 'react'
+
+interface CartaoProps {
+    bgcolor?: string
+    children?: any
+}
+
+export default function Cartao(props: CartaoProps) {
+    return (
+        <div className={styles.cartao} style={{backgroundColor: props.bgcolor ?? "#fff"}}>
+            {props.children}
+        </div>
+    )
+}
