fa8f4a511faa3c63c353f25af27cc0f309265d9b
Ruan-Portella <ruanmorales29@gmail.com>
2023-06-16 22:26:27 -0300

    Feat: Cria tela inicial do jogo

--- secao_02/portas/src/pages/index.tsx
+++ secao_02/portas/src/pages/index.tsx
@@ -1,14 +1,26 @@
-import React from "react";
-import { useState } from "react";
-import Porta from "../components/Porta";
-import PortaModel from "../model/porta";
-import { atualizarPortas, criarPortar } from "../functions/portas";
+import React, { useState } from "react";
+import Cartao from "../components/Cartao";
+import styles from '../styles/Formulario.module.css'
+import Link from "next/link";
+import EntradaNumerica from "../components/EntradaNumerica";
 
-export default function Home() {
-  
+export default function Formulario() {
+  const [qtdePortas, setQtdPortas] = useState(3)
+
   return (
-   <div style={{display: 'flex'}}>
-    <h1>Formulário de inicio</h1>
+   <div className={styles.formulario}>
+    <div>
+      <Cartao>
+        <EntradaNumerica value={qtdePortas} onChange={novaQtd => setQtdPortas(novaQtd)} text='Qtde Portas?'/>
+      </Cartao>
+    </div>
+    <div>
+      <Cartao bgcolor='#28a085'>
+        <Link href={`/jogo/${qtdePortas}`}>
+          <h2 className={styles.link}>Iniciar</h2>
+        </Link>
+      </Cartao>
+    </div>
    </div>
   )
 }
