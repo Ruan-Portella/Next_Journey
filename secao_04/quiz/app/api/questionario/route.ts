2177df551917175235ad40d2a552a64720cdb7ee
Ruan-Portella <ruanmorales29@gmail.com>
2023-06-18 15:44:41 -0300

    Feat: Utiliza função embaralhar na api de questões

--- secao_04/quiz/app/api/questionario/route.ts
+++ secao_04/quiz/app/api/questionario/route.ts
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server"
 import questoes from "../bancodeQuestoes"
+import { embaralhar } from "@/functions/arrays"
 
 export async function GET(request: any, {params}: any) {
         const questoesId = questoes.map(questao => questao.id)
-        return NextResponse.json({ questoesId }, { status: 202 })
+        return NextResponse.json( embaralhar(questoesId) , { status: 200 })
 }
