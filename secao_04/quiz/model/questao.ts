2a172c2c449de53a404b60a70817c0317c8f7a2c
Ruan-Portella <ruanmorales29@gmail.com>
2023-06-17 21:51:25 -0300

    Feat: Integra classe resposta com questao

--- secao_04/quiz/model/questao.ts
+++ secao_04/quiz/model/questao.ts
@@ -1,11 +1,12 @@
+import RespostaModel from "./resposta"
+
 export default class QuestaoModel {
     #id: number
     #enunciado: string
-    #respostas: any[]
+    #respostas: RespostaModel[]
     #acertou: boolean
-    // #respondida: boolean
 
-    constructor(id: number, enunciado: string, respostas: any[], acertou: boolean) {
+    constructor(id: number, enunciado: string, respostas: RespostaModel[], acertou: boolean) {
         this.#id = id
         this.#enunciado = enunciado
         this.#respostas = respostas
@@ -29,7 +30,9 @@ export default class QuestaoModel {
     }
 
     get respondida() {
-        //FIXME: implementar esse método
+        for (let resposta of this.#respostas) {
+            if (resposta.revelada) return true
+        }
         return false
     }
 
