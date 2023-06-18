084936e18da71f7a2e14cc1b9774c55a17dbb2d2
Ruan-Portella <ruanmorales29@gmail.com>
2023-06-17 21:14:19 -0300

    Feat: Cria componente Estático

---
+++ secao_03/renderizacao/app/estatico1/page.jsx
@@ -0,0 +1,10 @@
+export default function Estatico1() {
+    // const randomNumber = Math.random()
+
+    return (
+        <div>
+            <h1>Estático #01</h1>
+            {/* <h2>{randomNumber}</h2> */}
+        </div>
+    )
+}
