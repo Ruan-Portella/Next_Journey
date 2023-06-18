46846f30096c0e1226d553f95ce4b35b1b8f7d6c
Ruan-Portella <ruanmorales29@gmail.com>
2023-06-18 15:45:12 -0300

    Feat: Utiliza função embaralhar na api de resposta

--- secao_04/quiz/app/api/questoes/[id]/route.ts
+++ secao_04/quiz/app/api/questoes/[id]/route.ts
@@ -5,8 +5,8 @@ export async function GET(request: any, {params}: any) {
     const id = Number(params.id)
     const questaoSelecionada = questoes.filter(questao => questao.id === id)
     if (questaoSelecionada.length === 1) {
-        const questao = questaoSelecionada[0].converterParaObjeto()
-        return NextResponse.json({ questao }, { status: 200 })
+        const questao = questaoSelecionada[0].embaralharRespostas()
+        return NextResponse.json( questao.converterParaObjeto() , { status: 200 })
     } else {
         return NextResponse.json({ error: 'mensagem' }, { status: 404 })
     }
