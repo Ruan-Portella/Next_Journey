3753a3c8fc84329ed27bb1cedab33081d102ffdb
Ruan-Portella <ruanmorales29@gmail.com>
2023-06-17 23:10:10 -0300

    Feat: Cria primeira parte da API

--- secao_04/quiz/app/api/questoes/[id]/route.ts
+++ secao_04/quiz/app/api/questoes/[id]/route.ts
@@ -2,5 +2,12 @@ import { NextResponse } from "next/server"
 import questoes from "../../bancodeQuestoes"
 
 export async function GET(request: any, {params}: any) {
-    return NextResponse.json(questoes[0].converterParaObjeto())
+    const id = Number(params.id)
+    const questaoSelecionada = questoes.filter(questao => questao.id === id)
+    if (questaoSelecionada.length === 1) {
+        const questao = questaoSelecionada[0].converterParaObjeto()
+        return NextResponse.json({ questao }, { status: 200 })
+    } else {
+        return NextResponse.json({ error: 'mensagem' }, { status: 404 })
+    }
 }
