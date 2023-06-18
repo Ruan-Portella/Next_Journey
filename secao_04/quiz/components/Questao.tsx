b06019f3794b3af855449cc06a320bea2094c0b7
Ruan-Portella <ruanmorales29@gmail.com>
2023-06-18 16:49:37 -0300

    Feat: Cria função para utilizar componente Respos

--- secao_04/quiz/components/Questao.tsx
+++ secao_04/quiz/components/Questao.tsx
@@ -2,6 +2,7 @@ import styles from '../style/Questao.module.css'
 
 import QuestaoModel from "@/model/questao";
 import Enunciado from './Enunciado';
+import Resposta from './Resposta';
 
 interface QuestaoProps {
     valor: QuestaoModel
@@ -9,9 +10,22 @@ interface QuestaoProps {
 
 export default function Questao(props: QuestaoProps) {
     const questao = props.valor
+
+    function renderizarRespostas() {
+        return questao.respostas.map((resposta, i) => {
+            return <Resposta 
+                valor={resposta}
+                indice={i}
+                letra='A'
+                corLetra='#F2C866'
+            />
+        })
+    }
+
     return(
         <div className={styles.questao}>
             <Enunciado texto={questao.enunciado} />
+            {renderizarRespostas()}
         </div>
     )
 }
