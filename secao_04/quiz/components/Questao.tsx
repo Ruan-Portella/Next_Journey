6c2123301b6bce20e3f216f6da422a8e652f9e13
Ruan-Portella <ruanmorales29@gmail.com>
2023-06-18 16:30:30 -0300

    Feat: Cria componente Questão

---
+++ secao_04/quiz/components/Questao.tsx
@@ -0,0 +1,16 @@
+import styles from '../style/Questao.module.css'
+
+import QuestaoModel from "@/model/questao";
+
+interface QuestaoProps {
+    valor: QuestaoModel
+}
+
+export default function Questao(props: QuestaoProps) {
+    const questao = props.valor
+    return(
+        <div className={styles.questao}>
+            <h1>Questao</h1>
+        </div>
+    )
+}
