1f915aa4329ab0fdd397758d839736c4e1c737b2
Ruan-Portella <ruanmorales29@gmail.com>
2023-06-18 16:05:56 -0300

    Fix: Resolve problema na api de questões

--- secao_04/quiz/app/api/questoes/[id]/route.ts
+++ secao_04/quiz/app/api/questoes/[id]/route.ts
@@ -3,10 +3,10 @@ import questoes from "../../bancodeQuestoes"
 
 export async function GET(request: any, {params}: any) {
     const id = Number(params.id)
-    const questaoSelecionada = questoes.filter(questao => questao.id === id)
-    if (questaoSelecionada.length === 1) {
-        const questao = questaoSelecionada[0].embaralharRespostas()
-        return NextResponse.json( questao.converterParaObjeto() , { status: 200 })
+    const unicaQuestao = questoes.filter(questao => questao.id === id)
+    if (unicaQuestao.length === 1) {
+        const questaoSelecionada = unicaQuestao[0].embaralharRespostas()
+        return NextResponse.json( questaoSelecionada.converterParaObjeto() , { status: 200 })
     } else {
         return NextResponse.json({ error: 'mensagem' }, { status: 404 })
     }
