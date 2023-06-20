0829117e5b5f8c243a189e35a7e3b4f47b2cac9e
Ruan-Portella <ruanmorales29@gmail.com>
2023-06-20 17:58:08 -0300

    Feat: Introduz CountDown

--- secao_04/quiz/components/Temporizador.tsx
+++ secao_04/quiz/components/Temporizador.tsx
@@ -1,3 +1,4 @@
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 import styles from '../style/Temporizador.module.css'
 
 interface TemporizadorProps {
@@ -8,7 +9,16 @@ interface TemporizadorProps {
 export default function Temporizador(props: TemporizadorProps) {
     return (
         <div className={styles.temporizador}>
-
+            <CountdownCircleTimer 
+                duration={props.duracao}
+                size={120}
+                isPlaying
+                onComplete={props.tempoEsgotado}
+                colors={['#BCE596', '#F7B801', '#A30000']}
+                colorsTime={[10, 5, 0]}
+            >
+                {({ remainingTime }) => remainingTime}
+            </CountdownCircleTimer>
         </div>
     )
 }
