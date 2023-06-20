932b9facd8ee88d1143889a2bd6b79cb5d57ed5f
Ruan-Portella <ruanmorales29@gmail.com>
2023-06-20 18:55:17 -0300

    Feat: Cria componente Botão

---
+++ secao_04/quiz/components/Botao.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import styles from '../style/Botao.module.css'
+
+interface BotaoProps {
+    texto: string
+    href?: string
+    onClick?: (e: any) => void
+}
+
+export default function Botao(props: BotaoProps) {
+
+    function renderizarBotao() {
+        return (
+        <button className={styles.botao}
+            onClick={props.onClick}>
+            {props.texto}
+        </button>
+        )
+    }
+    return (
+        props.href ? (
+            <Link href={props.href}>
+                {renderizarBotao()}
+            </Link>
+        ) : renderizarBotao()
+    )
+};
