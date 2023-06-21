c6d57cfa805d8a1536d45a0efe6d1b4267727264
Ruan-Portella <ruanmorales29@gmail.com>
2023-06-21 15:58:31 -0300

    Feat: Cria função para fazer fetch nas questões

--- secao_04/quiz/app/page.tsx
+++ secao_04/quiz/app/page.tsx
@@ -2,7 +2,7 @@
 import Questionario from '@/components/Questionario'
 import QuestaoModel from '@/model/questao'
 import RespostaModel from '@/model/resposta'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const questaoMock = new QuestaoModel(1, 'Melhor cor?', [ 
   RespostaModel.errada('Verde'), 
@@ -11,8 +11,31 @@ const questaoMock = new QuestaoModel(1, 'Melhor cor?', [
   RespostaModel.certa('Preta'),
 ])
 
+const BASE_URL = 'http://localhost:3000/api'
+
 export default function Home() {
-  const [questao, setQuestao] = useState(questaoMock)
+  const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([]);
+  const [questao, setQuestao] = useState<QuestaoModel>(questaoMock)
+
+  async function carregarIdsQuestoes() {
+    const resp = await fetch(`${BASE_URL}/questionario`);
+    const idDasQuestoes = await resp.json();
+    setIdsDasQuestoes(idDasQuestoes);
+  }
+
+  async function carregarQuestao(idQuestao: number) {
+    const resp = await fetch(`${BASE_URL}/questoes/${idQuestao}`);
+    const json = await resp.json();
+    
+  }
+
+  useEffect(() => {
+    carregarIdsQuestoes();
+  }, [])
+
+  useEffect(() => {
+    idsDasQuestoes.length > 0 && carregarQuestao(idsDasQuestoes[0])
+  }, [idsDasQuestoes])
 
   function questaoRespondida(questao: QuestaoModel) {
 
@@ -23,13 +46,7 @@ export default function Home() {
   }
 
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      flexDirection: 'column',
-      alignItems: 'center',
-      height: '100vh'
-    }}>
+    <div>
       <Questionario 
         questao={questao}
         ultima={true}
