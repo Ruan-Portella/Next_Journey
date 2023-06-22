39bf9340935949fce50a473af16106eccc8f8a92
Ruan-Portella <ruanmorales29@gmail.com>
2023-06-21 21:33:14 -0300

    Feat: Cria componente Estatistica

---
+++ secao_04/quiz/components/Estatistica.tsx
@@ -0,0 +1,21 @@
+import styles from '../style/Estatistica.module.css'
+
+interface EstatisticaProps {
+    valor: any
+    texto: string
+    corFundo?: string
+    corFonte?: string
+}
+
+export default function Estatistica(props: EstatisticaProps) {
+    return (
+        <div className={styles.estatistica}>
+            <div className={styles.valor} style={{backgroundColor: props.corFundo ?? '#FDD60F', color: props.corFonte ?? `#333`}}>
+                {props.valor}
+            </div>
+            <div className={styles.texto}>
+                {props.texto}
+            </div>
+        </div>
+    )
+}
