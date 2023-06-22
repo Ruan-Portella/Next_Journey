64cc9d621b35a6690337c0ab2788c25dd99181ba
Ruan-Portella <ruanmorales29@gmail.com>
2023-06-21 21:15:33 -0300

    Feat: Cria chave unica para key Resposta

--- secao_04/quiz/components/Questao.tsx
+++ secao_04/quiz/components/Questao.tsx
@@ -23,7 +23,7 @@ export default function Questao(props: QuestaoProps) {
     function renderizarRespostas() {
         return questao.respostas.map((resposta, i) => {
             return <Resposta 
-                key={i}
+                key={`${questao.id}-${i}`}
                 valor={resposta}
                 indice={i}
                 letra={letras[i].valor}
@@ -36,7 +36,7 @@ export default function Questao(props: QuestaoProps) {
     return (
         <div className={styles.questao}>
             <Enunciado texto={questao.enunciado} />
-            <Temporizador duracao={props.tempoParaResposta ?? 15} tempoEsgotado={props.tempoEsgotado} />
+            <Temporizador key={questao.id}  duracao={props.tempoParaResposta ?? 15} tempoEsgotado={props.tempoEsgotado} />
             {renderizarRespostas()}
         </div>
     )
