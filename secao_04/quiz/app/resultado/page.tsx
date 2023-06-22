1fbb2f860542451ae67e12816e57d90da02f9ad7
Ruan-Portella <ruanmorales29@gmail.com>
2023-06-21 21:23:48 -0300

    Feat: Pega valores da url e mostra no resultado

--- secao_04/quiz/app/resultado/page.tsx
+++ secao_04/quiz/app/resultado/page.tsx
@@ -1,7 +1,20 @@
+'use client' 
+import styles from '../../style/Resultado.module.css'
+import { useSearchParams } from 'next/navigation'
+
 export default function resultado() {
+    const searchParams = useSearchParams()
+
+    const total = searchParams.get('total')
+    const certas = searchParams.get('certas')
+    const percentual = Math.round((Number(certas) / Number(total)) * 100);
+
     return (
-        <div>
-            Resultado
+        <div className={styles.resultado}>
+            <h1>Resultado</h1>
+            <div>{total}</div>
+            <div>{certas}</div>
+            <div>{`${percentual}%`}</div>
         </div>
     )
 }
