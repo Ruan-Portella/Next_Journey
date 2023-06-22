37f7d3fe5d188bc25b5a0b000dcc503d36d33f08
Ruan-Portella <ruanmorales29@gmail.com>
2023-06-21 21:46:54 -0300

    Feat: Cria Style para errado e certo

--- secao_04/quiz/components/Resposta.tsx
+++ secao_04/quiz/components/Resposta.tsx
@@ -1,50 +1,48 @@
-import RespostaModel from "@/model/resposta"
-import styles from '../style/Resposta.module.css'
+import RespostaModel from "@/model/resposta";
+import styles from "../style/Resposta.module.css";
 
 interface RespostaProps {
-    valor: RespostaModel
-    indice: number
-    letra: string
-    corFundoLetra: string
-    respostaFornecida: (indice: number) => void
+  valor: RespostaModel;
+  indice: number;
+  letra: string;
+  corFundoLetra: string;
+  respostaFornecida: (indice: number) => void;
 }
 
 export default function Resposta(props: RespostaProps) {
-    const resposta = props.valor
-    
-    return (
-        <div className={styles.resposta} id="resposta" onClick={() => props.respostaFornecida(props.indice)} >
-            <div className={styles.conteudoResposta}>
-                {
-                    !resposta.revelada ? (
-                        <div className={styles.frente}>
-                        <div className={styles.letra}
-                             style={{ backgroundColor: props.corFundoLetra }}>
-                            {props.letra}
-                        </div>
-                        <div className={styles.valor}>
-                            {resposta.valor}
-                        </div>
-                    </div>
-                    ) : (
-                        <div className={styles.verso}>
-                        {
-                            resposta.certa ? (
-                             <div className={styles.certa}>
-                                <div>A reposta está certa</div>
-                                <div className={styles.valor} >{resposta.valor}</div>
-                             </div>
-                            ) : (
-                             <div className={styles.errada}>
-                                 <div>A reposta está errada</div>
-                                <div className={styles.valor} >{resposta.valor}</div>
-                             </div>
-                           )
-                        }
-                    </div>
-                    )
-                }
-            </div>
+  const resposta = props.valor;
+  const respostaRevelada = resposta.revelada ? styles.respostaRevelada : "";
+
+  return (
+    <div
+      className={styles.resposta}
+      id="resposta"
+      onClick={() => props.respostaFornecida(props.indice)}
+    >
+      <div className={`${respostaRevelada} ${styles.conteudoResposta}`}>
+        <div className={styles.frente}>
+          <div
+            className={styles.letra}
+            style={{ backgroundColor: props.corFundoLetra }}
+          >
+            {props.letra}
+          </div>
+          <div className={styles.valor}>{resposta.valor}</div>
         </div>
-    )
+        <div className={styles.verso}>
+          {resposta.certa ? (
+            <div className={styles.certa}>
+              <div>A reposta está certa</div>
+              <div className={styles.valor}>{resposta.valor}</div>
+            </div>
+          ) : (
+            <div className={styles.errada}>
+              <div>A reposta está errada</div>
+              <div className={styles.valor}>{resposta.valor}</div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
